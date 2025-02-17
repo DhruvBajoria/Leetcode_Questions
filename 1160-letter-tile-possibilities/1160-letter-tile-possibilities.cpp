b4c72a01526f475e92214c12c2487af9fd99f45f
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2025-02-17 19:42:51 +0530

    Time: 26 ms (47.83%), Space: 25.1 MB (6.58%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(vector<int>freq){
+    int ways =0;
+    for(int i=0;i<26;i++){
+        if(freq[i]>0){
+            freq[i]--;
+            ways+=1+solve(freq);
+            freq[i]++;
+        }
+    }
+    return ways;
+}
+    int numTilePossibilities(string tiles) {
+        vector<int>freq(26,0);
+        for(auto x:tiles){
+            freq[x-'A']++;
+        }
+        return solve(freq);
+    }
+};
