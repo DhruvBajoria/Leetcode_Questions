3540864d2ed972139def9adad638018965288859
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2025-03-03 17:29:00 +0530

    Time: 7 ms (59.22%), Space: 133.5 MB (51.98%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        queue<int>s,e,g;
-        int n=nums.size();
+        // queue<int>s,e,g;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot)
+        //     s.push(nums[i]);
+        //     else if(nums[i]==pivot)
+        //     e.push(nums[i]);
+        //     else{
+        //         g.push(nums[i]);
+        //     }
+        // }
+        // vector<int>ans;
+        // while(!s.empty()){
+        //     ans.push_back(s.front());
+        //     s.pop();
+        // }
+        //   while(!e.empty()){
+        //     ans.push_back(e.front());
+        //     e.pop();
+        // }
+        //   while(!g.empty()){
+        //     ans.push_back(g.front());
+        //     g.pop();
+        // }
+        // return ans;
+        vector<int>ans;
+        int n= nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]<pivot)
-            s.push(nums[i]);
-            else if(nums[i]==pivot)
-            e.push(nums[i]);
-            else{
-                g.push(nums[i]);
-            }
+            ans.push_back(nums[i]);
         }
-        vector<int>ans;
-        while(!s.empty()){
-            ans.push_back(s.front());
-            s.pop();
+         for(int i=0;i<n;i++){
+            if(nums[i]==pivot)
+            ans.push_back(nums[i]);
         }
-          while(!e.empty()){
-            ans.push_back(e.front());
-            e.pop();
-        }
-          while(!g.empty()){
-            ans.push_back(g.front());
-            g.pop();
+         for(int i=0;i<n;i++){
+            if(nums[i]>pivot)
+            ans.push_back(nums[i]);
         }
         return ans;
     }
